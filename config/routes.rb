98eeaee785a9d7c29b1b9bfdab58f844c8e4b2d3
Aziz <sharipov.aziz.sh@gmail.com>
2015-08-26 06:59:43 +0000

    Added Sign in/out/up

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Nomster2::Application.routes.draw do
-  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -55,6 +54,6 @@ Nomster2::Application.routes.draw do
   #     resources :products
   #   end
   root 'places#index'
-  
   resources :places 
+  devise_for :users
 end
