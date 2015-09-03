ed062bdc0eb66283d1ee8ae261d8a1da2e152ac2
Aziz <sharipov.aziz.sh@gmail.com>
2015-09-03 12:58:59 +0000

    Fixed all bugs

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,7 @@ class NotificationMailer < ActionMailer::Base
   default from: "no-reply@nomsterapp.com"
 
   def comment_added
-  	 mail(to: "aziz_veyron@inbox.ru",
+  	 mail(to: "aziz.dev.ruby@gmail.com",
          subject: "A comment has been added to your place")
   end
 end
--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,4 +1,7 @@
 Geocoder.configure({
         :lookup => :bing,
+        :timeout => 10,
         :api_key => ENV['GEOCODER_API_KEY']
+
+
 })
