ed7b49cecb4c89cd276be3bd1f026019f7ac3ee7
Aziz <sharipov.aziz.sh@gmail.com>
2015-09-08 03:31:52 +0000

    Fixed bug with My profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :places
-  has_many :comments
+  has_many :places   
+  has_many :comments, through: :places
   has_many :photos
 end
